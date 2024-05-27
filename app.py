59c8ea3b797df01bf70e515ed992b461877824e9
Malay Kumar <malaykumar2003@gmail.com>
2024-05-27 21:47:21 +0530

    Feat: Preference to urls from user history

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from forms import UrlForm
-from autocomplete import correct_and_autocomplete_url
+from autocomplete import correct_and_autocomplete_url, set_history
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test'
 CORS(app)
@@ -46,5 +46,14 @@ def post_ext():
         # Handle the case where "url" key is not present in the request.json
         return jsonify(error=str(e))
 
+@app.route("/post_history", methods=["POST"])
+def post_history():
+    try:
+        history = request.json["data"]
+        result = set_history(history=history)
+        return jsonify(result= "data recieved")
+    except KeyError as e:
+        return jsonify(error=str(e))
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
