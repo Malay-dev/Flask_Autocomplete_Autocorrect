df2543239662fc555165b49a6b1ce1aba029d625
Malay Kumar <malaykumar2003@gmail.com>
2024-05-29 12:46:26 +0530

    Feat: Refresh Dataset

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
 from forms import UrlForm
 from autocomplete import correct_and_autocomplete_url, set_history
+from refresh_dataset import retrieve_data
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test'
 CORS(app)
@@ -55,5 +56,14 @@ def post_history():
     except KeyError as e:
         return jsonify(error=str(e))
     
+@app.route("/refresh_dataset", methods=["POST"])
+def refresh_dataset():
+    try:
+        retrieve_data()
+        return jsonify(result="Dataset refreshed successfully")
+    except KeyError as e:
+        return jsonify(error=str(e))
+    
+
 if __name__ == "__main__":
     app.run(debug=True, port=8080)
---
+++ refresh_dataset.py
@@ -0,0 +1,61 @@
+import pandas as pd
+from pymongo import MongoClient
+from pandas import json_normalize
+import tldextract
+
+def replace_nan_for_column(dataframe, column_name, replacement_value):
+    dataframe[column_name].fillna(replacement_value, inplace=True)
+    return dataframe
+
+def combine_columns_to_new_url_column(dataframe, protocol_column, domain_column, new_column_name):
+    dataframe[new_column_name] = dataframe.apply(
+        lambda row: f"{row[protocol_column]}//{row[domain_column]}/" if row[protocol_column] else row[domain_column],
+        axis=1
+    )
+    return dataframe
+
+def set_default_subdomain(url):
+    ext = tldextract.extract(url)
+    if ext.subdomain == '':
+        return f"www.{url}"
+    return url
+
+def extract_info(url):
+    ext = tldextract.extract(url)
+    subdomain = ext.subdomain
+    domain = ext.domain
+    suffix = ext.suffix
+    return subdomain, domain, suffix
+
+mongo_uri = "mongodb+srv://admin:gTYc5AEnB9PK6GuI@url-dataset-cluster.kfsuavg.mongodb.net/?retryWrites=true&w=majority"
+client = MongoClient(mongo_uri)
+
+database_name = "test"
+collection_name = "datas"
+
+def retrieve_data():
+    db = client[database_name]
+    collection = db[collection_name]
+    cursor = collection.find()
+    fields_to_include = [
+        'url_protocol',
+        'url_domain_name',
+        'metadata_location',
+        'metadata_global_index',
+        'metadata_local_index',
+        'metadata_category_rank.rank',
+        'metadata_category_rank.category',
+    ]
+    df = json_normalize(list(cursor), sep='_').filter(items=fields_to_include)
+    df = df.dropna(subset=['metadata_global_index'])
+    df['url_domain_name'] = df['url_domain_name'].apply(set_default_subdomain)
+    df[['subdomain', 'domain', 'suffix']] = df['url_domain_name'].apply(lambda x: pd.Series(extract_info(x)))
+
+    df = replace_nan_for_column(df, "metadata_location", "IN")
+    df = combine_columns_to_new_url_column(df, "url_protocol", "url_domain_name", "url")
+
+    csv_filename = "data.csv"
+    df.to_csv(csv_filename)
+    client.close()
+
+    print(f"Data exported to {csv_filename}")
